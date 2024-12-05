0ee816cf2906e0cad02836869919cf5519c84639
Killian Ca <killiancarty@gmail.com>
2024-12-05 11:31:17 +0000

    Adding mentor contract and tests

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -4,6 +4,8 @@ const contract = require("@truffle/contract");
 const StudentRegistryArtifact = require("../../build/contracts/StudentRegistry.json");
 const InterviewShareArtifact = require("../../build/contracts/InterviewShare.json");
 const CVShareArtifact = require("../../build/contracts/CVShare.json");
+const MentorshipProgramArtifact = require("../../build/contracts/MentorshipProgram.json");
+
 
 const provider = ganache.provider();
 const web3 = new Web3(provider);
@@ -11,14 +13,18 @@ const web3 = new Web3(provider);
 const StudentRegistry = contract(StudentRegistryArtifact);
 const InterviewShare = contract(InterviewShareArtifact);
 const CVShare = contract(CVShareArtifact);
+const MentorshipProgram = contract(MentorshipProgramArtifact);
+
 StudentRegistry.setProvider(provider);
 InterviewShare.setProvider(provider);
 CVShare.setProvider(provider);
+MentorshipProgram.setProvider(provider);
 
 module.exports = {
   web3,
   StudentRegistry,
   InterviewShare,
   CVShare,
+  MentorshipProgram,
   provider,
 };
